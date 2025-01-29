dd196477f3f59cb985a96e9cf67398da1bde5b03
AkinyiFO <oafridah@gmail.com>
2025-01-29 20:38:58 +0300

    "Initial"

--- src/main/java/dev/labs/s3/Arguments.java
+++ src/main/java/dev/labs/s3/Arguments.java
@@ -3,42 +3,27 @@ package dev.labs.s3;
 public class Arguments {
 
     public static void main(String[] args) {
-        Arguments testInt = new Arguments();
-        int passInMain = 6;
-        testInt.primitiveDataType(passInMain); // Passed in by value
-        System.out.println(passInMain + " set in main" + "\n");
+        System.out.println(Circle.numberOfCircles); // Referenced by class name only as it is a class variable
 
-        String passedInMain = "The argument";
-        testInt.referenceDataTypes(passedInMain);
-        System.out.println(passedInMain + "\n");
+        Circle oval = new Circle(5,5);
+        System.out.println(Circle.numberOfCircles);
 
-        int originOneMain = 10;
-        int originTwoMain = 10;
-        Circle myCircle = new Circle(originOneMain, originTwoMain);
-        System.out.println("Passed in arguments for originOne is " + originOneMain + ", and originTwo is " + originTwoMain);
-        myCircle.moveCircle(myCircle, 5, 5);
-        System.out.println("originOne change persisted as " + myCircle.getOriginOne() + " and originTwo changes persisted as " + myCircle.getOriginTwo() + "\n");
+        Circle round = new Circle(1,1);
+        System.out.println(Circle.numberOfCircles);
+
+        System.out.println(round.numberOfCircles); // Possible but discouraged because it is not clear we are accessing a class variable
     }
-
-    void primitiveDataType(int passedInTheMethod) {
-        System.out.println(passedInTheMethod + " is the argument");
-        passedInTheMethod = 10; // Value changes are only within the scope of the method
-        System.out.println(passedInTheMethod + " reassigned in method");
-    }
-
-    void referenceDataTypes(String passIn) {
-        System.out.println(passIn);
-        passIn = "Passed in method";
-        System.out.println(passIn);
-    }
-
 }
 
 // Ref: https://docs.oracle.com/javase/tutorial/java/javaOO/arguments.html
 class Circle {
     private int originOne;
     private int originTwo;
-    private static int numberOfCircles = 0;
+
+    // Class Variable
+    public static int numberOfCircles = 0;
+
+    static final double PI = 3.1416;
 
     public Circle(int originOne, int originTwo) {
         this.originOne = originOne;
@@ -46,12 +31,10 @@ class Circle {
         ++numberOfCircles;
     }
 
-    public Circle() {
-    }
-
-    // For consistency, and easy maintenance, use one constructor for initialization when overloading constructors
-    public Circle(int origin) {
-        this(origin, origin);
+    public static int getNumberOfCircles(){
+//        return originOne; // Not possible
+//        int test = new Circle(2,2).originOne; // Work around
+        return numberOfCircles;
     }
 
     public int getOriginOne() {
@@ -70,15 +53,5 @@ class Circle {
         this.originTwo = originTwo;
     }
 
-    public void moveCircle(Circle circle, int originOne, int originTwo) {
-        // code to move origin of circle to x+deltaX, originTwo+deltaY
-        System.out.println("moveCircle passed in arguments for originOne is " + originOne + ", and originTwo is " + originTwo);
-        circle.setOriginOne(circle.getOriginOne() + originOne); // persist even when method returns
-        circle.setOriginTwo(circle.getOriginTwo() + originTwo); // persists even when method returns
-        System.out.println("Updated originOne is " + circle.getOriginOne() + ", and originTwo is " + circle.getOriginTwo());
 
-        // code to assign a new reference to circle - won't persist
-        circle = new Circle(0, 0); // Passed in by value does not persist
-        System.out.println("originOne reassigned in the method as " + circle.getOriginOne() + ", and originTwo as " + circle.getOriginTwo());
-    }
 }
